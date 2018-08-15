0a992cb28f89078222d2fdaad59983f9d84edd7d
isaacrdz <zack_cpp@hotmail.com>
2018-08-14 22:50:46 -0500

    redux workflow before dispatch

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -46,3 +46,15 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 export default connect(mapStateToProps)(Landing);
+import React, { Component } from 'react'
+
+ class Landing extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default Landing;
